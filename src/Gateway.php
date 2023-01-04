a2e6c66282664afd7ea8c25a9a2a61673c8f92ee
Kamil Socha <68702708+ShoeRiderr@users.noreply.github.com>
2023-01-04 18:10:27 +0100

    Update Gateway.php

--- src/Gateway.php
+++ src/Gateway.php
@@ -1,60 +1,10 @@
 <?php
-/**
- * Voucher Gateway
- */
+
 namespace Omnipay\Voucher;
 
 use Omnipay\Common\AbstractGateway;
+use Omnipay\Voucher\src\Message\Request;
 
-/**
- * Voucher Gateway
- *
- * This gateway is useful for processing check or direct debit payments. It simply
- * authorizes every payment.
- *
- * ### Example
- *
- * #### Initialize Gateway
- *
- * ```php
- * // Create a gateway for the Voucher Gateway
- * // (routes to GatewayFactory::create)
- * $gateway = Omnipay::create('Voucher');
- *
- * // Initialise the gateway
- * $gateway->initialize(array(
- *     'testMode' => true, // Or false. Doesn't matter.
- * ));
- * ```
- *
- * #### Authorize
- *
- * <code>
- * // Do a purchase transaction on the gateway
- * try {
- *     $transaction = $gateway->authorize(array(
- *         'amount'        => '10.00',
- *         'currency'      => 'AUD',
- *         'description'   => 'This is a test transaction.',
- *       ));
- *     $response = $transaction->send();
- *     $data = $response->getData();
- *     echo "Gateway authorize response data == " . print_r($data, true) . "\n";
- *
- *     if ($response->isSuccessful()) {
- *         echo "Transaction was successful!\n";
- *     }
- * } catch (\Exception $e) {
- *     echo "Exception caught while attempting authorize.\n";
- *     echo "Exception type == " . get_class($e) . "\n";
- *     echo "Message == " . $e->getMessage() . "\n";
- * }
- * </code>
- *
- * In reality, Voucher Gateway authorize() requests will always be successful and
- * will never throw an exception, but the above example shows that you can treat
- * manual payments just like any other payment type for any other gateway.
- */
 class Gateway extends AbstractGateway
 {
     public function getName()
@@ -62,43 +12,9 @@ class Gateway extends AbstractGateway
         return 'Voucher';
     }
 
-    public function getDefaultParameters()
-    {
-        return array();
-    }
-
-    public function authorize(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Voucher\Message\Request', $parameters);
-    }
-
     public function purchase(array $parameters = array())
     {
-        return $this->createRequest('\Omnipay\Voucher\Message\Request', $parameters);
-    }
-
-    public function completePurchase(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Voucher\Message\Request', $parameters);
-    }
-
-    public function completeAuthorise(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Voucher\Message\Request', $parameters);
-    }
-
-    public function capture(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Voucher\Message\Request', $parameters);
-    }
-
-    public function void(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Voucher\Message\Request', $parameters);
-    }
-
-    public function refund(array $parameters = array())
-    {
-        return $this->createRequest('\Omnipay\Voucher\Message\Request', $parameters);
+        return $this->createRequest(Request::class, $parameters);
     }
 }
+
