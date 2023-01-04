6783010a6be148ee4ac4cde2343c2b017ffd7bd1
Kamil Socha <68702708+ShoeRiderr@users.noreply.github.com>
2023-01-04 18:10:53 +0100

    Update Request.php

--- src/Message/Request.php
+++ src/Message/Request.php
@@ -1,21 +1,13 @@
 <?php
-/**
- * Voucher Gateway Request
- */
 
 namespace Omnipay\Voucher\Message;
 
 use Omnipay\Common\Message\AbstractRequest;
 
-/**
- * Voucher Gateway Request
- */
 class Request extends AbstractRequest
 {
     public function getData()
     {
-        $this->validate('amount');
-
         return $this->getParameters();
     }
 
